2e9a581e06ec3ccf59e156a92561a06a02552ef9
saeed joulaei <saeedjoulaei86@gmail.com>
2023-07-30 11:10:53 +0330

    first section added to project

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const newnumber = 18 + 2;
-console.log(newnumber);
+const choices = ["rock", "paper", "scissors"];
+const userChoice = prompt("choose rock, paper,scissors");
+if (userChoice) {
+  console.log(`you choosed: ${userChoice}`);
+} else {
+  console.log("you cheated!!");
+}
+const randomNumber = Math.floor(Math.random() * 3);
+const computerChoices = choices[randomNumber];
+console.log(`computer choosed: ${computerChoices}`);
