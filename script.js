3a2fa86336e8a2224a9ba8c481884c91623eb9c1
saeed joulaei <saeedjoulaei86@gmail.com>
2023-07-30 13:12:54 +0330

    project completed

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ if (userChoice) {
 const randomNumber = Math.floor(Math.random() * 3);
 const computerChoices = choices[randomNumber];
 console.log(`computer choosed: ${computerChoices}`);
+if (userChoice === computerChoices) {
+  console.log("it is a tie");
+} else if (userChoice === "rock") {
+  if (computerChoices === "scissors") {
+    console.log("you won !");
+  } else {
+    console.log("computer won !");
+  }
+} else if (userChoice === "paper") {
+  if (computerChoices === "scissors") {
+    console.log("computer won !");
+  } else {
+    console.log("you won !");
+  }
+} else if (userChoice === "scissors") {
+  if (computerChoices === "rock") {
+    console.log("computer won !");
+  } else {
+    console.log("you won !");
+  }
+}
